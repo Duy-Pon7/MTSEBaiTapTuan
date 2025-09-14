8bd32f8674741de91f506439a79e14c7814ce478
Truong Quoc Duy <duytrquoc@gmail.com>
2025-09-14 23:25:06 +0700

    Fix UI

--- BaiTap05/ExpessJS01/src/routes/api.js
+++ BaiTap05/ExpessJS01/src/routes/api.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const { createUser, handleLogin, getUser, getAccount } = require('../controllers/userController');
-const { getLessons } = require('../controllers/lessonController');
-const auth = require('../middleware/auth');
-const delay = require('../middleware/delay');
+const express = require("express");
+const {
+  createUser,
+  handleLogin,
+  getUser,
+  getAccount,
+} = require("../controllers/userController");
+const { getLessons } = require("../controllers/lessonController");
+const auth = require("../middleware/auth");
+const delay = require("../middleware/delay");
 
 const routerAPI = express.Router();
 
 routerAPI.use(auth);
 
 routerAPI.get("/", (req, res) => {
-  return res.status(200).json("Hello Phan Duy Kiet" );
-})
+  return res.status(200).json("Hello Trương Quốc Duy");
+});
 
 routerAPI.post("/register", createUser);
 routerAPI.post("/login", handleLogin);
